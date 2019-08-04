233e7906856f261323a410b4cc878ee5c59881bc
15sora2 <stgordon19@gmail.com>
2019-08-04 13:33:19 -0500

    Created Customer object, tested for Naming functionality

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Tom",
+                LastName = "Sawyer"
+            };
+
+            var expectedFullName = "Sawyer, Tom";
+
+            //Act (perform the operation(s) we are testing)
+            var actualfullName = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expectedFullName, actualfullName);
+        }
+
+        [TestMethod]
+        public void EmptyFirstNameInFullName()
+        {
+            Customer customer = new Customer
+            {
+                LastName = "Sawyer"
+            };
+
+            string expectedFullName = "Sawyer";
+
+            string actualFullName = customer.FullName;
+
+            Assert.AreEqual(actualFullName, expectedFullName);
+        }
+
+        [TestMethod]
+        public void EmptyLastNameInFullName()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Tom"
+            };
+
+            string expectedFullName = "Tom";
+
+            string actualFullName = customer.FullName;
+
+            Assert.AreEqual(actualFullName, expectedFullName);
+        }
+    }
+}
---
+++ ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("402a09e8-de43-481b-905f-facd9e5fd639")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
