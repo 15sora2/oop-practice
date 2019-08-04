3fa67448b6fa8a105cd6d604a376c32460d00d5c
15sora2 <stgordon19@gmail.com>
2019-08-04 18:51:44 -0500

    Added Validate, save, and retrieve methods. Tested Validate method

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -12,14 +12,17 @@ namespace ACM.BL
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public int CustomerId { get; private set; }
-        public string FullName {
+        public string FullName
+        {
             get
             {
                 string fullName = $"{LastName}, {FirstName}";
                 if (string.IsNullOrWhiteSpace(LastName))
                 {
                     fullName = FirstName;
-                } else if(string.IsNullOrWhiteSpace(FirstName)){
+                }
+                else if (string.IsNullOrWhiteSpace(FirstName))
+                {
                     fullName = LastName;
                 }
                 return fullName;
@@ -27,5 +30,42 @@ namespace ACM.BL
         }
 
         public static int instanceCount { get; set; }
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName) ||
+            string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Retrieve a list of all customers
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            //todo: Retrieve customer data
+
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Saves the current user
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            //todo: Save defined customer
+
+            return true;
+        }
     }
 }
